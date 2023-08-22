70a83ff329deeefd96148df8e25494a8159cbc88
Yunus Emre Deligöz <ye@deligoz.me>
2023-08-23 02:11:15 +0300

    Refactor autoload and improve code readability
    Removed the 'autoload' section from composer.json and modified the 'plugin-modifies-downloads' key in the 'extra' section to 'class', enabling the SharedScriptsPlugin. Also, slightly refactored the getCapabilities() method in SharedScriptsPlugin for better readability. This change helps to simplify the code and streamline dependency management.
    
    Signed-off-by: Yunus Emre Deligöz <ye@deligoz.me>

--- src/SharedScriptsPlugin.php
+++ src/SharedScriptsPlugin.php
@@ -4,16 +4,12 @@ declare(strict_types=1);
 
 namespace Phonyland\DevTools;
 
-use Composer\Composer;
-use Composer\IO\IOInterface;
 use Composer\Plugin\Capability\CommandProvider;
 use Composer\Plugin\Capable;
-use Composer\Plugin\PluginInterface;
 
 class SharedScriptsPlugin implements  Capable
 {
-
-    public function getCapabilities()
+    public function getCapabilities(): array
     {
         return [
             CommandProvider::class => CustomCommandProvider::class
