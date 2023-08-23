9969b5325d9c99a7a819d34ef83be98c29953df7
Yunus Emre Deligöz <ye@deligoz.me>
2023-08-23 09:00:21 +0300

    Move LintCommand file and update linting execution
    The LintCommand file was moved to /src/Commands directory to improve project structure. The linting execution was updated to call the pint executable and its configuration from the correct path. This ensures the linting command runs properly. Also added new aliases for the lint command and defined return type for the configure function for better code clarity and organization.
    
    Signed-off-by: Yunus Emre Deligöz <ye@deligoz.me>

--- src/Commands/LintCommand.php
+++ src/Commands/LintCommand.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 
-namespace Phonyland\DevTools;
+namespace Phonyland\DevTools\Commands;
 
 use Composer\Command\BaseCommand;
 use Symfony\Component\Console\Input\InputInterface;
@@ -11,16 +11,21 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class LintCommand extends BaseCommand
 {
-    protected function configure()
+    protected function configure(): void
     {
         $this->setName('lint2');
+        $this->setAliases(['lint3, lint4']);
         $this->setDescription('Run linting using shared logic.');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        // Your linting logic here
-        system("@pint");
+        // Define the path to the pint executable and its configuration
+        $pintExecutable = getcwd() . '/vendor/bin/pint';
+        $configPath = getcwd() . '/vendor/phonyland/dev-tools/src/pint.json';
+
+        // Run the linting command
+        system("$pintExecutable --config=$configPath");
 
         return 0; // Return 0 for success
     }
